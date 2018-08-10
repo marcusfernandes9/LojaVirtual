512390bedc7bff4568ef36cf5ecf2cc3f9a27f6f
Unknown <marcus.fernandes9@gmail.com>
2018-08-09 21:42:24 -0400

    Crud de Produtos

---
+++ LojaVirtual/Controllers/ProdutosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LojaVirtual.Models;
+using System.Data.Entity.Core.Objects;
+using LojaVirtual.Extensions;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private LojaVirtualContext db = new LojaVirtualContext();
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View(db.Produtos.ToList());
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Preco,Descricao")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                ObjectParameter retorno = new ObjectParameter("retorno", typeof(string));
+
+                Entities db = new Entities();
+                db.sp_ins_produto(produto.Nome,produto.Preco,produto.Descricao, retorno);
+
+                db.SaveChanges();
+                return RedirectToAction("Index").Success("Produto inserido com sucesso!");
+            }
+
+            return View(produto);
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Preco,Descricao")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto).Success("Produto editado com sucesso!");
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produto produto = db.Produtos.Find(id);
+            db.Produtos.Remove(produto);
+            db.SaveChanges();
+            return RedirectToAction("Index").Success("Produto excluido com sucesso!");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 LojaVirtual
+
+Descrição: 	Foi criado um CRUD para cadastro de produtos para simular uma pagina de ecommerce.
+			A inserção dos registros de produtos é realizado através de uma store procedure (sp_ins_produto). 
+
+End point: /p	rodutos
+
+Tecnologias Utilizadas:
+	- Asp.Net MVC 5
+	- Entity Framework
+	- Bootstrap
+	- Jquery
+	
+Banco de dados: Sql Server Local(.mdf)
